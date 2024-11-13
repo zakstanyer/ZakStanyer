bc476847d69b99c2a582d9b1f39679303aa8a8d9
Zak Stanyer <zs262342@falmouth.ac.uk>
2024-11-13 21:22:46 +0000

    df

--- obj/Debug/net8.0/ZakStanyer.AssemblyInfo.cs
+++ obj/Debug/net8.0/ZakStanyer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZakStanyer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f655ff0de0132755407d30c6dd896dd64977725")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZakStanyer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZakStanyer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
